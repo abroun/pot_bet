5097c1a469a2c360573b1ac32eaa976de9fe51c3
Alan Broun <abroun@alanbroun.net>
2015-06-28 15:46:42 +0100

    Really added the TODO list and made a few changes to get deployment to work

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,12 @@ from blog.models import BlogPost
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index( request ):
+    
+    if request.user.is_authenticated():
+        print ">>>>>>> User is authenticated"
+    else:
+        print ">>>>>>> User is NOT authenticated"
+    
     return render_to_response( 'blog/index.html', {
         'posts' : BlogPost.objects.all()[:5]
     })
