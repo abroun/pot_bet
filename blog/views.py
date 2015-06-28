900e03d3f6be92ff7f53e553afa2a3a54c3d1fb0
Alan Broun <abroun@alanbroun.net>
2015-06-28 16:42:28 +0100

    Added in a navbar and set the page title

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from blog.models import BlogPost
 from django.shortcuts import render_to_response, get_object_or_404
 
+#---------------------------------------------------------------------------------------------------
+class PageInfo:
+    """Helper class which holds the name of an important page along with a link
+       to the page"""
+    
+    def __init__( self, name, link ):
+        self.name = name
+        self.link = link
+
+PAGE_LIST = [
+    PageInfo( "Home", "/" ),
+    PageInfo( "Admin", "/admin" )
+]
+
+#---------------------------------------------------------------------------------------------------
 def index( request ):
     
     if request.user.is_authenticated():
@@ -9,10 +24,15 @@ def index( request ):
         print ">>>>>>> User is NOT authenticated"
     
     return render_to_response( 'blog/index.html', {
-        'posts' : BlogPost.objects.all()[:5]
+        "pageList" : PAGE_LIST,
+        "activePageName" : "Home",
+        "posts" : BlogPost.objects.all()[:5]
     })
 
+#---------------------------------------------------------------------------------------------------
 def view_post( request, slug ):   
-    return render_to_response( 'blog/view_post.html', {
-        'post' : get_object_or_404( BlogPost, slug=slug )
+    return render_to_response( "blog/view_post.html", {
+        "pageList" : PAGE_LIST,
+        "activePageName" : None,
+        "post" : get_object_or_404( BlogPost, slug=slug )
     })
