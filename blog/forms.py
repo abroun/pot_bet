9c662e421e94b7bcce8cf9ef57c85d2eed04aaa9
Alan Broun <abroun@alanbroun.net>
2015-06-30 11:14:06 +0100

    Added Bootstrap styping to the Add Post page

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 from blog.models import BlogPost
 
 #---------------------------------------------------------------------------------------------------
@@ -7,4 +7,9 @@ class BlogPostForm( ModelForm ):
     class Meta:
         model = BlogPost
         fields = [ "title", "slug", "text", "tags" ]
-
+        widgets = {
+            "title" : TextInput( attrs={ "class" : "form-control" } ),
+            "slug" : TextInput( attrs={ "class" : "form-control" } ),
+            "text" : Textarea( attrs={ "class" : "form-control" } ),
+            "tags" : TextInput( attrs={ "class" : "form-control" } )
+        }
